982ba5435b812d80888fcd3e011f9bb02a0d6dff
Sergei2000 <dsdem@mail.ru>
2020-05-18 17:19:57 +0300

    2

---
+++ include/process.h
@@ -0,0 +1,14 @@
+//
+// Created by sergei on 16.05.2020.
+//
+
+#ifndef LAB11_PROCESS_H
+#define LAB11_PROCESS_H
+#include <iostream>
+#include <boost/process/shell.hpp>
+#include <boost/process.hpp>
+#include <async++.h>
+#include <vector>
+#include <signal.h>
+#include <boost/program_options.hpp>
+#endif //LAB11_PROCESS_H
---
+++ sources/sources.cpp
@@ -0,0 +1,97 @@
+// Copyright 2018 Your Name <your_email>
+
+#include <header.hpp>
+#include <process.h>
+#include <constants.h>
+
+int exit_code;
+std::vector <pid_t> processes;
+bool pack = false;
+bool done = false;
+bool install = false;
+int seconds = 0;
+
+
+int main(int argc, char **argv) {
+    std::string config;
+    try {
+        boost::program_options
+        ::options_description desc("Allowed options");
+        desc.add_options()
+                ("config", boost::program_options
+                ::value<std::string>(), "configuration")
+                ("pack", "package")
+                ("install", "_install")
+                ("timeout", boost::
+                program_options::value<int>(), "timing");
+
+        boost::
+        program_options::variables_map vm;
+        store
+        (parse_command_line(argc, argv, desc), vm);
+        notify(vm);
+
+        if (vm.count("config")) {
+            config = vm["config"].as<std::string>();
+            std::cout << "config:" << config << std::endl;
+        } else {
+            config = "Debug";
+        }
+        if (vm.count("pack")) {
+            std::cout << "package" << std::endl;
+            pack = true;
+        } else {
+            pack = false;
+        }
+        if (vm.count("install")) {
+            install = true;
+            std::cout << "install" << std::endl;
+        } else {
+            install = false;
+        }
+        if (vm.count("timeout")) {
+            seconds = vm["timeout"].as<int>();
+        } else {
+            seconds = 0;
+        }
+    }
+    catch (...) {
+        std::cout << "lazha" << "\n";
+    }
+    if (seconds) {
+        auto task = async::spawn([] {
+            sleep(seconds);
+            for (auto a: processes) {
+                kill(a, SIGINT);
+            }
+        });
+    }
+    auto task1 = async::spawn([] {
+        std::error_code ec;
+        processes.push_back
+        (boost::this_process::get_id());
+        config = building + config;
+        exit_code = boost::process::system
+                (config, ec);
+        processes.pop_back();
+    });
+    auto task2 = task1.then([] {
+        if ((install) && (exit_code == 0)) {
+            processes.push_back
+            (boost::this_process::get_id());
+            exit_code = boost::
+                    process::system(installing);
+            processes.pop_back();
+        }
+    });
+    auto task3 = task2.then([] {
+        if ((pack) && (exit_code == 0)) {
+            processes.push_back
+            (boost::this_process::get_id());
+            exit_code = boost::process::system
+                    (packing);
+            processes.pop_back();
+        }
+    });
+    return 0;
+}
